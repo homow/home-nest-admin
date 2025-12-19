556ac2ef4971518f477dc45bc13376184eeb940e
homow <homowsh897@gmail.com>
2025-12-19 21:39:58 +0330

    create custom theme

---
+++ src/components/CustomThemeProvider.tsx
@@ -0,0 +1,14 @@
+import {ThemeProvider} from 'next-themes';
+import {type ChildrenComponentProps} from "@/types/ui"
+
+export default function CustomThemeProvider({children}: ChildrenComponentProps) {
+    return (
+        <ThemeProvider
+            enableSystem
+            attribute="class"
+            defaultTheme="system"
+        >
+            {children}
+        </ThemeProvider>
+    );
+}
