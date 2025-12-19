2428f4e1173179f8783f4ef8908b71920c7e2a52
homow <homowsh897@gmail.com>
2025-12-19 21:40:21 +0330

    update Home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import CustomThemeProvider from "@/components/CustomThemeProvider";
 import {ChildrenComponentProps} from "@/types/ui";
+import {dana, geist} from "@/lib/fonts";
 import "@/styles/globals.css";
 
 export default function RootLayout(
@@ -7,11 +9,40 @@ export default function RootLayout(
     }: ChildrenComponentProps
 ) {
     return (
-        <html lang="fa" dir="rtl">
+        <html
+            lang="fa"
+            dir="rtl"
+            className={`${dana.variable} ${geist.variable}`}
+            suppressHydrationWarning
+        >
+        <head>
+            {/* <=== meta tags ===> */}
+            <meta charSet="UTF-8"/>
+            <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+            {/* <=== icons ===> */}
+            <link rel="manifest" href="/favicon/site.webmanifest"/>
+            <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png"/>
+            <link rel="shortcut icon" href="/favicon/favicon.ico"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+            <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png"/>
+            <link rel="icon" type="image/png" sizes="512x512" href="/favicon/android-chrome-512x512.png"/>
+
+            {/* <=== title ===> */}
+            <title>پنل مدیریت | آشیانه</title>
+        </head>
         <body
             className={`antialiased`}
         >
-        {children}
+        <CustomThemeProvider>
+            <header></header>
+            <main>
+                {children}
+            </main>
+            <footer></footer>
+        </CustomThemeProvider>
         </body>
         </html>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
     return (
-        <div>Home</div>
+        <div className={"p-20"}>
+            <h1>نسخه خانه</h1>
+            <h1>Home</h1>
+            <h1 className={"font-geist"}>نسخه خانه</h1>
+            <h1 className={"font-geist"}>Home</h1>
+        </div>
     );
 };
