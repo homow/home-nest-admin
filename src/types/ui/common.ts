46545f4d468991a6219b418ebe2835fbb6c51700
homow <homowsh897@gmail.com>
2025-12-20 18:54:12 +0330

    add more types

--- src/types/ui/common.ts
+++ src/types/ui/common.ts
@@ -1,5 +1,9 @@
 import type {ReactNode} from "react";
 
-export type ChildrenComponentProps =  Readonly<{
+export type MainComponentProps =  Readonly<{
     children?: ReactNode;
+    className?: string;
 }>;
+
+export type NoLimitArgsFn = (...args: unknown[]) => unknown;
+export type UseToggleFn = (value?: boolean) => void;
--- src/types/ui/index.ts
+++ src/types/ui/index.ts
@@ -1,2 +1,3 @@
 export * from "./common";
 export * from "./side-bar";
+export * from "./top-bar";
