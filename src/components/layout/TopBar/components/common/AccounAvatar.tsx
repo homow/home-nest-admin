20868a5a64fac950f95756a2939640a9c9e627dd
homow <homowsh897@gmail.com>
2025-12-20 07:24:16 +0330

    add AccounAvatar.tsx

---
+++ src/components/layout/TopBar/components/common/AccounAvatar.tsx
@@ -0,0 +1,33 @@
+import {cn} from "@/lib/utils/ui-utils";
+import Image from "next/image";
+import OnlineUi from "./OnlineUi";
+
+interface Props {
+    className?: string;
+}
+
+export default function AccountAvatar(
+    {
+        className,
+    }: Props
+) {
+    return (
+        <div
+            className={cn(
+                "relative size-10 rounded-full",
+                className
+            )}
+        >
+            <Image
+                preload
+                quality={80}
+                width={80}
+                height={80}
+                alt="آواتار اکانت"
+                className={"w-full"}
+                src="/images/shared/Avatar.webp"
+            />
+            <OnlineUi/>
+        </div>
+    );
+};
