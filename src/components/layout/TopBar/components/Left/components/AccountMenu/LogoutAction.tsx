4c6e7bdfb318524359e66262a69239e783f9d8e3
homow <homowsh897@gmail.com>
2025-12-22 01:28:41 +0330

    add a component for logout

---
+++ src/components/layout/TopBar/components/Left/components/AccountMenu/LogoutAction.tsx
@@ -0,0 +1,10 @@
+import AlertModal from "@/components/modals/AlertModal";
+import ConfirmModal from "@/components/modals/ConfirmModal";
+
+export default function LogoutAction() {
+    return (
+        <div>
+
+        </div>
+    );
+};
