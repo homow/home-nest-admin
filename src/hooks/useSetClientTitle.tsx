fde71da06054f1fe7ce799686a7c2a1d74041219
homow <homowsh897@gmail.com>
2025-12-22 18:49:36 +0330

    update

--- src/hooks/useSetClientTitle.tsx
+++ src/hooks/useSetClientTitle.tsx
@@ -2,8 +2,10 @@
 
 import {useEffect} from "react";
 
+const baseTitle: string = "آشیانه"
+
 export default function useSetClientTitle(title: string) {
     useEffect(() => {
-        document.title = `${title} | آشیانه`;
+        document.title = `${title} | ${baseTitle}`;
     }, [title]);
 };
