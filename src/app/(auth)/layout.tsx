72e31a48d13f8c317befde3de90adf92d6f76cd4
homow <homowsh897@gmail.com>
2025-12-22 08:15:43 +0330

    fix layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import type {MainComponentProps} from "@/types/ui";
+
+export default function Layout({children}: MainComponentProps) {
+    return (
+        <section className="flex-1">
+            {children}
+        </section>
+    );
+};
