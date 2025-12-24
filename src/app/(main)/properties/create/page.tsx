5bfaa7d3a45e0272261eff3dcb28fe3febce7e68
homow <homowsh897@gmail.com>
2025-12-24 07:07:24 +0330

    create properties create page

---
+++ src/app/(main)/properties/create/page.tsx
@@ -0,0 +1,15 @@
+import type {Metadata} from 'next';
+
+export default function CreateProperties() {
+    return (
+        <section
+            className={"main-components"}
+        >
+            CreateProperties
+        </section>
+    );
+};
+
+export const metadata: Metadata = {
+    title: `ساخت ملک جدید ${process.env.NEXT_PUBLIC_BASE_TITLE}`,
+};
