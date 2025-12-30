06d4b9c14ae998fc9ebdc173dbf97f8229eadcd9
homow <homowsh897@gmail.com>
2025-12-30 19:21:17 +0330

    clean(codes): clean codes on eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,12 @@
+import stylistic from "@stylistic/eslint-plugin";
 import nextTs from "eslint-config-next/typescript";
 import {defineConfig, globalIgnores} from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
-import stylistic from "@stylistic/eslint-plugin";
 
 const eslintConfig = defineConfig([
     ...nextVitals,
     ...nextTs,
-    // Override default ignores of eslint-config-next.
     globalIgnores([
-        // Default ignores of eslint-config-next:
         ".next/**",
         "out/**",
         "build/**",
