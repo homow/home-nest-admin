0311ca2f3f79757a132c23d442ea7e8f01d69580
homow <homowsh897@gmail.com>
2025-12-31 21:32:01 +0330

    add(queryClientProvier): create react query client provider

--- src/components/clientWrapper/ReactQueryProvider.tsx
+++ src/components/clientWrapper/ReactQueryProvider.tsx
@@ -1,2 +1,17 @@
 "use client";
 
+import {MainComponentProps} from "@/types/ui";
+import queryClient from "@/lib/api/configs/react-query";
+import {QueryClientProvider} from "@tanstack/react-query";
+
+export default function ReactQueryProvider(
+    {
+        children
+    }: MainComponentProps
+) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
+};
