f97f8c5b8935e2878604742850ca50502637958e
homow <homowsh897@gmail.com>
2026-01-01 22:06:50 +0330

    update(auth): update api route for refresh

--- src/app/api/auth/refresh/route.ts
+++ src/app/api/auth/refresh/route.ts
@@ -1,6 +1,18 @@
-import {NextRequest} from "next/server";
-import
+import {NextRequest, NextResponse} from "next/server";
+import {cookies} from "next/headers";
 
 export async function POST(req: NextRequest) {
+    const cookieStore = await cookies();
+    const refreshToken = cookieStore.get("refreshToken");
+
+    if (!refreshToken) {
+        return NextResponse.json({
+            ok: false,
+            message: "refresh token is expired",
+        }, {
+            status: 401
+        });
+    }
+
 
 }
